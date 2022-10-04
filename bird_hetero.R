7c943628cf6970dd1ec775d02388357688f3af14
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-10-03 17:27:46 -0700

    Update bird_hetero.R

--- bird_hetero.R
+++ bird_hetero.R
@@ -3,5 +3,9 @@ bird_het <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEP
 ggplot(bird_het,aes(x=reorder(`Organism Name`,Corrected_HET),y=Corrected_HET)) + 
     geom_bar(stat='identity') + 
     coord_flip() + labs(y='Heterozygosity',x='')+theme_classic()
-    
+
+OR:
+
+ggplot(bird_het,aes(x=reorder(`Organism Name`,Corrected_HET),y=Corrected_HET)) + 
++     geom_segment(aes(xend=`Organism Name`,yend=0)) + geom_point()+ labs(y='Heterozygosity',x='')+theme_classic()+coord_flip()
     
