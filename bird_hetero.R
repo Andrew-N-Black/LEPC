c0d446de853ebe40e79b49d5f34e4987c22170b4
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-10-03 17:22:03 -0700

    Create bird_hetero.R

---
+++ bird_hetero.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+bird_het <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/bird_het.xlsx")
+ggplot(bird_het,aes(x=reorder(`Organism Name`,Corrected_HET),y=Corrected_HET)) + 
+    geom_bar(stat='identity') + 
+    coord_flip() + labs(y='Heterozygosity',x='')+theme_classic()
+    
+    
