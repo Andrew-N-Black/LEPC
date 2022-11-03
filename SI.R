0834ceb39e4024d5ecb1014852174f220c33f350
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-11-03 14:23:05 -0700

    Update SI.R

--- SI.R
+++ SI.R
@@ -10,3 +10,17 @@ geom_vline(xintercept = 1300)+geom_vline(xintercept = 2500)
 ggsave("S1.svg")
 
 
+#Plotting Depth:
+
+depth_breadth <- read_excel("~/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/depth_breadth.xlsx")
+ggplot(data=depth_breadth, aes(y=filt.d, x=reorder(ID,filt.d),fill=HABITAT))+
++     geom_bar(stat="identity")+scale_fill_manual(title1, values =c("Shinnery-Oak-Prairie"="bisque","Mixed-Grass-Prairie"="blue","Sand-Sagebrush-Prairie"="darkorchid1","Shortgrass-CRP-Mosaic"="darkolivegreen3"))+ylab("Coverage")+xlab("Bird")+theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
+
+ggsave("cov.svg")
+
+#Mapping rate
+ggplot(data=depth_breadth, aes(y=map, x=reorder(ID,map),fill=HABITAT))+
++     geom_bar(stat="identity")+scale_fill_manual(title1, values =c("Shinnery-Oak-Prairie"="bisque","Mixed-Grass-Prairie"="blue","Sand-Sagebrush-Prairie"="darkorchid1","Shortgrass-CRP-Mosaic"="darkolivegreen3"))+ylab("Reads Aligned (%)")+xlab("Bird")+theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
+
+ggsave("mapping.svg")
+
