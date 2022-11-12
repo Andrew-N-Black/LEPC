f5e1fed8113ea67457921e9c0eca25031610e4c0
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-11-12 14:23:32 -0800

    Update relatedness_lek.R

--- relatedness_lek.R
+++ relatedness_lek.R
@@ -9,12 +9,13 @@ ggplot(lek_kinship, aes(GROUP, COUNT))+geom_bar(aes(fill = GROUP), position = "d
 #Better sorted plot showing relatedness within each lek
 library(forcats)
 library(ggplot2)
-kin<-read_xlsx("scaffold1-results_TRUE.xlsx")
+kin <- read_excel("top6_results1_match.xlsx")
+#kin<-read_xlsx("scaffold1-results_TRUE.xlsx")
 kin$KIN <- factor(kin$KIN, names(sort(table(kin$KIN), decreasing = TRUE)))
-ggplot(data =df, aes(forcats::fct_infreq(LEK_A)))+geom_bar(aes(fill=KIN))+coord_flip()+xlab("")+ylab("Pairwise Comparisons")+theme_classic()+coord_flip()+ theme(axis.text = element_text(size = 4)) +xlab("Lek")+scale_fill_manual(title, values =c("DUPLICATE"="bisque","PO"="blue","HS"="darkorchid1","FC"="darkolivegreen3","UR"="black"))
+ggplot(data =kin, aes(forcats::fct_infreq(LEK_A)))+geom_bar(aes(fill=KIN))+coord_flip()+xlab("")+ylab("Pairwise Comparisons")+theme_classic()+coord_flip()+ theme(axis.text = element_text(size = 4)) +xlab("Lek")+scale_fill_manual("Kinship", values =c("DUPLICATE"="bisque","PO"="blue","HS"="darkorchid1","FC"="darkolivegreen3","UR"="black"))
 
 #Or kinship based upon ecotype
-ggplot(data =df, aes(forcats::fct_infreq(HABITAT_A)))+geom_bar(aes(fill=KIN))+coord_flip()+xlab("")+ylab("Pairwise Comparisons")+theme_classic()+coord_flip()+ theme(axis.text = element_text(size = 10)) +xlab("Ecotype")+scale_fill_manual(title, values =c("DUPLICATE"="bisque","PO"="blue","HS"="darkorchid1","FC"="darkolivegreen3","UR"="black"))
+ggplot(data =kin, aes(forcats::fct_infreq(HABITAT_A)))+geom_bar(aes(fill=KIN))+coord_flip()+xlab("")+ylab("Pairwise Comparisons")+theme_classic()+coord_flip()+ theme(axis.text = element_text(size = 10)) +xlab("")+scale_fill_manual("Kinship", values =c("DUPLICATE"="bisque","PO"="blue","HS"="darkorchid1","FC"="darkolivegreen3","UR"="black"))
 
 #Can change the order of the kinship factor and use as input for above if needed
 df <- kin[order(kin$KIN,decreasing = TRUE),]
