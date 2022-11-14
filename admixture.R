d233664f924f4e18687d0fe9bddd83c5d5b386da
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-11-14 14:24:56 -0800

    Update admixture.R

--- admixture.R
+++ admixture.R
@@ -5,7 +5,7 @@ library(pophelper)
 library(readxl)
 setwd("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis")
 metadata <- read.csv("metadata.csv")
-group <- as.data.frame(metadata[,11])
-#group <- metadata[,c(2,3,8,10,11)]
-k2<-readQ("~/all_pca_356.admix.2.Q")
-plotQ(qlist=k2,returnplot=T,exportplot=T,clustercol=c("black","darkgrey"),grplab=group,ordergrp=T,showlegend=T,showtitle=T,showsubtitle=T,titlelab="LEPC",subtitlelab="population structure",height=1.6,indlabsize=1.8,indlabheight=0.08,indlabspacer=-1,barbordercolour="white",divsize = 0.1,grplabsize=1,barbordersize=0,linesize=0.4,showsp = T,splabsize = 0,outputfilename="plotq",imgtype="png",exportpath=getwd())
+both <- as.data.frame(metadata[,c(11,3)])
+slist<-readQ(files =c("~/all_pca_356.admix.2.Q","~/all_pca.admix.4.Q","~/all_pca.admix.3.Q"))
+plotQ(slist[1:3],returnplot=T,exportplot=T,imgoutput = "join",clustercol=c("blue","bisque","darkolivegreen3","darkorchid1"),grplab=both,ordergrp=T,showlegend=T,showtitle=T,showsubtitle=T,titlelab="LEPC",subtitlelab="population structure",height=1.6,indlabsize=1.8,indlabheight=0.08,indlabspacer=-1,barbordercolour="white",divsize = 0.1,grplabsize=1,barbordersize=0,linesize=0.4,showsp = T,splabsize = 0,outputfilename="plotq",imgtype="png",exportpath=getwd())
+
