653605fe1b2e43e5fd9bc6074ab69500c9b026b2
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-11-14 17:06:32 -0800

    Update inbreed.R

--- inbreed.R
+++ inbreed.R
@@ -1,3 +1,9 @@
 library(ggplot2)
-sampleinbreed <- read_excel("sampleinbreed.xltx")
-ggplot(sampleinbreed,aes(x=reorder(LEK,FIS),y=FIS,fill=HABITAT))+geom_boxplot(show.legend =FALSE,outlier.shape = NA)+scale_fill_manual(title, values =c("Shinnery-Oak-Prairie"="bisque","Mixed-Grass-Prairie"="blue","Sand-Sagebrush-Prairie"="darkorchid1","Shortgrass-CRP-Mosaic"="darkolivegreen3"))+xlab("")+ylab("Inbreeding")+theme_classic()+coord_flip()
+inbreed_filt <- read.csv("~/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/inbreed_filt.csv")
+ggplot(inbreed_filt,aes(x=reorder(LEK,FIS),y=FIS,fill=HABITAT))+geom_boxplot(show.legend =FALSE,outlier.shape = NA)+scale_fill_manual(title, values =c("Shinnery-Oak-Prairie"="bisque","Mixed-Grass-Prairie"="blue","Sand-Sagebrush-Prairie"="darkorchid1","Shortgrass-CRP-Mosaic"="darkolivegreen3"))+xlab("")+ylab("Inbreeding")+theme_classic()+coord_flip()+geom_abline(intercept = 0.00,slope=0)
+ggsave("inbreeding_lek.svg")
+ggsave("inbreeding_lek.pdf")
+
+ggplot(inbreed_filt,aes(x=reorder(HABITAT,FIS),y=FIS,fill=HABITAT))+geom_boxplot(show.legend =FALSE,outlier.shape = NA)+scale_fill_manual(title, values =c("Shinnery-Oak-Prairie"="bisque","Mixed-Grass-Prairie"="blue","Sand-Sagebrush-Prairie"="darkorchid1","Shortgrass-CRP-Mosaic"="darkolivegreen3"))+xlab("")+ylab("Inbreeding")+theme_classic()+coord_flip()+geom_abline(intercept = 0.00,slope=0)
+ggsave("inbreeding.svg")
+ggsave("inbreeding.pdf")
