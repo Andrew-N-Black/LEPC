59a98969e9e9ca7a73bafe810328f049ed3583b9
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-11-16 09:51:16 -0800

    Update relatedness_lek.R

--- relatedness_lek.R
+++ relatedness_lek.R
@@ -1,5 +1,10 @@
 library(ggplot2)
 library(readxl)
+top6_results1 <- read_excel("top6_results1.xlsx")
+ggplot(top6_results1,aes(x = KING, y = R0,group = KIN)) +
+    geom_point(aes(col = KIN))+scale_color_manual("Kinship", values =c("Duplicates"="bisque","Parent-Offspring"="blue","Half-Siblings"="darkorchid1","Full-Cousins"="darkolivegreen3","Unrelated"="black"))+theme_classic2()
+
+
 lek_kinship <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/lek_kinship.xlsx")
 lek_kinship$GROUP<-factor(lek_kinship$GROUP,levels = c("Within","Between"))
 ggplot(lek_kinship, aes(GROUP, COUNT))+geom_bar(aes(fill = GROUP), position = "dodge", stat="identity")+facet_wrap(~KINSHIP,scales = "free_y",ncol = 5)+scale_fill_manual("Lek", values =c("Within"="black","Between"="blue"))+xlab("")+ylab("Pairwise Comparisons")+theme_classic()+ theme(axis.text = element_text(size = 6)) +xlab("Lek")+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())
