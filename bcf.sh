74e7580fd2c9c5b1bbe84aef7a70a4a1a0a4996d
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-11-18 10:58:53 -0800

    Update bcf.sh

--- bcf.sh
+++ bcf.sh
@@ -2,8 +2,9 @@
 #assuming bcftools roh output file is called "ROH_LEPC_PLraw.txt, extract read groups:
 awk '$1=="RG"' ROH_LEPC_PLraw.txt > ROH_RG_all.txt
 
+#FOR INDIVIDUAL ROH
 #Now extract each samples row data and export to a seperate file
-for i in `cat sample.names`; do  grep $i ROH_RG_all.txt  > $i.ROH.txt ; done
+for i in `cat sample.list`; do  grep $i ROH_RG_all.txt  > $i.ROH.txt ; done
 
 #Parse each output file
 for i in `ls -1 *ROH.txt`; do python ROHparser.py $i > ${i}_results.txt ; done
@@ -11,4 +12,11 @@ for i in `ls -1 *ROH.txt`; do python ROHparser.py $i > ${i}_results.txt ; done
 cat *_results.txt | grep "F(ROH)" | cut -d " " -f 4| cut -f 1 > 100kb
 #Extract the fROH >1MB
 cat *_results.txt | grep "F(ROH)" | cut -d " " -f 7| cut -f 1 > 1MB
+#Get sample order
+ls -1 *results.txt | cut -d "_" -f 1 > samps
+#create file for figure
+paste samps 100kb 1MB > ind_roh.txt
+
+
+
 
