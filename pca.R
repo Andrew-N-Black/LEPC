620e41e0d6e13fb9923ffb9d5281de774b7664c0
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-11-18 09:51:12 -0800

    Update pca.R

--- pca.R
+++ pca.R
@@ -2,15 +2,15 @@ library(readxl)
 metadata <- read.csv("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/metadata.csv")
 View(metadata)                              
 library(ggplot2)
-cov<-as.matrix(read.table("scaf_22_pca.cov"))
+cov<-as.matrix(read.table("~/all_pca.cov"))
 axes<-eigen(cov)
 axes$values/sum(axes$values)*100
-  [1] 3.81590616 1.04967471 0.94312197 0.78195581 0.76650334
+  [1] 3.72338077 1.06535360 0.97923585 
 
 PC1_3<-as.data.frame(axes$vectors[,1:3])
 title1<-"Habitat"
-title2<-"Species"
+#title2<-"Species"
 
-ggplot(data=PC1_3, aes(y=V2, x=V1, shape=as.factor(metadata$SPECIES),color=as.factor(metadata$HABITAT)))+geom_point(size=2)+ theme_classic() + xlab("PC1 (3.75%)") +ylab("PC2 (1.06%)")+scale_color_manual(title1, values =c("Shinnery-Oak-Prairie"="bisque","Mixed-Grass-Prairie"="blue","Sand-Sagebrush-Prairie"="darkorchid1","Shortgrass-CRP-Mosaic"="darkolivegreen3"))+geom_hline(yintercept=0,linetype="dashed")+geom_vline(xintercept =0,linetype="dashed")+scale_shape_manual(title2,values=c(20,18))
+ggplot(data=PC1_3, aes(y=V2, x=V1, shape=as.factor(metadata$SPECIES),color=as.factor(metadata$HABITAT)))+geom_point(size=2)+ theme_classic() + xlab("PC1 (3.72%)") +ylab("PC2 (1.06%)")+scale_color_manual(title1, values =c("Shinnery-Oak-Prairie"="bisque","Mixed-Grass-Prairie"="blue","Sand-Sagebrush-Prairie"="darkorchid1","Shortgrass-CRP-Mosaic"="darkolivegreen3"))+geom_hline(yintercept=0,linetype="dashed")+geom_vline(xintercept =0,linetype="dashed")+scale_shape_manual(title2,values=c(20,18))
 
  
