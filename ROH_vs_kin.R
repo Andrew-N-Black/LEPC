4f99063e7c76b6ba8bd44ac64c1dad6d8f43988d
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-11-18 16:23:12 -0800

    Create ROH_vs_kin.R

---
+++ ROH_vs_kin.R
@@ -0,0 +1,10 @@
+top6_results1 <- read_excel("~/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/top6_results1.xlsx")
+                             
+idab<-new_roh_ind[match(top6_results1$idb, new_roh_ind$ID),]
+
+idaa<-new_roh_ind[match(top6_results1$ida, new_roh_ind$ID),]
+
+ID_A<-cbind(idaa,top6_results1$KIN)
+ID_A<-cbind(idaa,top6_results1$KIN,top6_results1$HABITAT_A)
+ID_B<-cbind(idab,top6_results1$KIN,top6_results1$HABITAT_B)
+ 
