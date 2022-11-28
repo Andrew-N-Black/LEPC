9275a9cd1d18e3bb8a396ee915e436451b4d5bca
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-11-28 13:10:41 -0800

    Update bird_hetero.R

--- bird_hetero.R
+++ bird_hetero.R
@@ -1,11 +1,12 @@
 library(readxl)
 library(ggplot2)
-bird_het <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/bird_het.xlsx")
+bird_het <- read_excel("bird_het.xlsx")
 ggplot(bird_het,aes(x=reorder(`Organism Name`,Corrected_HET),y=Corrected_HET)) + 
     geom_bar(stat='identity') + 
     coord_flip() + labs(y='Heterozygosity',x='')+theme_classic()
 
 #OR:
 
-ggplot(bird_het,aes(x=reorder(CommonName,Corrected_HET),y=Corrected_HET)) +  geom_segment(aes(xend=CommonName,yend=0)) + geom_point(size=3)+ labs(y='Heterozygosity',x='')+theme_classic()+coord_flip()
+ggplot(bird_het,aes(x=reorder(CommonName,Corrected_HET),y=Corrected_HET)) +  geom_segment(aes(xend=CommonName,yend=0)) + geom_point(size=6)+ labs(y='Heterozygosity',x='')+theme_classic()+coord_flip()
+ggsave("bird_het.svg")
 
