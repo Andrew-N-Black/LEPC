d9ff3f458bfb91476f5f71bec69b3fcb2abb3a8a
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-11-28 09:08:35 -0800

    Update ROH.R

--- ROH.R
+++ ROH.R
@@ -1,7 +1,7 @@
 
 #By individual
-roh<-x[c(2,5,6,7,8,11,16,17)]
-melt_data<-melt(roh,id.vars = c("ID","LEK","SEX","AGE","STATE","HABITAT"))
+FILTERED_ROH_IND <- read.csv("~/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/LEPC/analysis/FILTERED_ROH_IND.csv")
+melt_data<-melt(FILTERED_ROH_IND,id.vars = c("ID","HABITAT"))
 
 ggplot(melt_data, aes(fill=variable, y=value, x=reorder(ID,value))) +geom_bar(stat="identity",position="dodge")+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+theme_classic()+facet_grid(~HABITAT,scales = "free_x")+
 scale_fill_manual(values=c("#9933FF","darkred"))
