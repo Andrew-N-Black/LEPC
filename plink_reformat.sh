7c795b63aa9b5d257561d94adf895c333db4a143
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-12-11 11:25:31 -0800

    Update plink_reformat.sh

--- plink_reformat.sh
+++ plink_reformat.sh
@@ -1,3 +1,10 @@
 #Convert tped to ped and reformat for use in parentage:
 plink --tfile scaffold32 --recode tab --allow-extra-chr --out TEST/scaffold.32
 paste names genos > formated_input.txt
+
+library(outliers)
+source("./apparent.R")
+InputFile <- read.table(file="./formated_input.txt",sep="\t",h=F)
+apparentOUT <- apparent(InputFile, MaxIdent=0.10, alpha=0.01, nloci=17, self=TRUE, plot=TRUE, Dyad=FALSE)
+
+
