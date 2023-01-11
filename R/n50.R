357f5ac65b94c594dbba4e5fd53a0faf703c30c3
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-01-10 21:59:12 -0800

    Update n50.R

--- R/n50.R
+++ R/n50.R
@@ -2,9 +2,8 @@ library(ggplot2)
 library(scales)
 metrics <- read_excel("metrics.xlsx")
 n50<-ggplot(metrics,aes(x=reorder(c.name,N50),y=N50,color=busco.c.percent,shape=level))  +  geom_segment(aes(xend=c.name,yend=0)) + geom_point(size=6)+ labs(y='Scaffold N50',x='')+theme_classic()+coord_flip()
-
 n50+scale_colour_gradient2(
-    high = "red",
-    mid = "black",
-    low = "grey",
-    midpoint = 80)
+    high = "black",
+    mid = "lightgrey",
+    low = "brown",
+    midpoint = 85)
